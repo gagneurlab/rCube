1f6a2256a9b530ff5adf513f8c1dbf0347c876f7
carinademel <cdemel@mpibpc.mpg.de>
2017-07-19 18:02:55 +0200

    fixing bug

--- R/summarizeRates.R
+++ R/summarizeRates.R
@@ -6,7 +6,7 @@
 #' @param featureRates An \code{rCubeRate object}, see constructor function 
 #'  \code{\link{createRResultCubeRates}}.
 #' @param topLevelFeatures \code{GRanges} annotation
-#' @param by grouping method ## TODO, now not used, was this what we intended to do?
+#' @param by grouping method, one of 'mean', 'median'
 #'
 #' @return Returns a \code{rCubeExperiment} object which contains averaged rates
 #' for the specified \code{topLevelFeatures}.
@@ -24,28 +24,30 @@ summarizeRates <- function(featureRates, topLevelFeatures, by=c('mean','median')
     rows <- rowRanges(featureRates)
     rates <- assay(featureRates)
     ov <- findOverlaps(rows, topLevelFeatures)
-    
-    mergedRates <- .createRResultCubeRatesTopLevelFeatures(featureRates, topLevelFeatures, replicate)
-    
+    mergedRates <- .createRResultCubeRatesTopLevelFeatures(featureRates,
+                                                            topLevelFeatures)
     mergeRates <- function(subject, rates, ov, by){
         hits <- queryHits(ov)[subjectHits(ov) == subject]
-        return(apply(rates, 2, mean, na.rm=TRUE))#match.fun(by))) then na.rm does not work
+        return(apply(rates[hits,], 2, match.fun(by), na.rm=TRUE))
     }
-    res <- t(sapply(unique(subjectHits(ov)), mergeRates, rates=rates, ov=ov, by=by))
-    assay(mergedRates) <- res
+    res <- t(sapply(unique(subjectHits(ov)), mergeRates, rates=rates, ov=ov, 
+                    by=by))
+    assay(mergedRates)[unique(subjectHits(ov)),] <- res
     
     return(mergedRates)
 }
 
 
 ## helper function to extract same rCubeRates setup as in featureRates but modify rowRanges
-.createRResultCubeRatesTopLevelFeatures <- function(featureRates, topLevelFeatures, replicate)
+.createRResultCubeRatesTopLevelFeatures <- function(featureRates, topLevelFeatures)
 {
-    dm = colData(featureRates)
+    dm <- colData(featureRates)
     rates <- matrix(NA, nrow=length(topLevelFeatures), ncol=nrow(dm))
     
-    se <- SummarizedExperiment(assays=list("rates"=rates), rowRanges=topLevelFeatures, colData=dm)
-    colnames(se) = dm$sample
+    se <- SummarizedExperiment(assays=list("rates"=rates),
+                               rowRanges=topLevelFeatures,
+                               colData=dm)
+    colnames(se) <- dm$sample
     se <- new("rCubeRates", se)
     return(se)
 }
