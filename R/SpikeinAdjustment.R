3ee33ae0acb5445cfc14cd6ab7294e451bdbe33f
carinademel <cdemel@mpibpc.mpg.de>
2017-07-27 13:57:38 +0200

    starting with spike-in adjustment

---
+++ R/SpikeinAdjustment.R
@@ -0,0 +1,33 @@
+adjustSynthesisRate <- function(spikeinCounts, featureRates, ratio_4sU = 0.1){
+    #molecular weights:
+    Mw_A = 329.2
+    Mw_U = 306.2
+    Mw_4sU = 322.26 #U + S - O: 306.2 + 32.06 - 16 = 322.26
+    Mw_C = 305.2
+    Mw_G = 345.2
+    Mw_triposphate = 159 # 5' triphosphate
+    
+    if(!is.null(metadata(spikeinCounts)$ratio_4sU)){
+        ratio_4sU = metadata(spikeinCounts)$ratio_4sU
+    }
+    data(spikeinGenome)
+    nucleotide.counts = Biostrings::letterFrequency(spikein.genome, c("A", "C", "G", "T"))
+    # spikein.molecular.weight = c(nucleotide.counts[c("chrS2","chrS4","chrS8"),"A"] * Mw_A +
+    #                                  (1-ratio_4sU) * nucleotide.counts[c("chrS2","chrS4","chrS8"),"T"] * Mw_U +
+    #                                  nucleotide.counts[c("chrS2","chrS4","chrS8"),"C"] * Mw_C +
+    #                                  nucleotide.counts[c("chrS2","chrS4","chrS8"),"G"] * Mw_G + 
+    #                                  ratio_4sU * nucleotide.counts[c("chrS2","chrS4","chrS8"),"T"] * Mw_4sU + 
+    #                                  Mw_triposphate,
+    #                              nucleotide.counts[c("chrS5","chrS9","chrS12"),"A"]*Mw_A + nucleotide.counts[c("chrS5","chrS9","chrS12"),"T"]*Mw_U + nucleotide.counts[c("chrS5","chrS9","chrS12"),"C"]*Mw_C + nucleotide.counts[c("chrS5","chrS9","chrS12"),"G"]*Mw_G + Mw_triposphate)
+    #for labeled spike-ins
+    extended.nucleotide.counts = cbind(nucleotide.counts, nucleotide.counts[,"T"])
+    #unlabeled spikeins
+    spikein.molecular.weight = c (rowSums(t(t(nucleotide.counts[as.character(seqnames(spikeinCounts))[rowData(spikeinCounts)$labeledSpikein == FALSE], ]) * c(Mw_A, Mw_C, Mw_G, Mw_U))),
+       rowSums(t(t(extended.nucleotide.counts[as.character(seqnames(spikeinCounts))[rowData(spikeinCounts)$labeledSpikein == TRUE], ]) * c(Mw_A, Mw_C, Mw_G, (1-ratio_4sU) * Mw_U, ratio_4sU*Mw_4sU)))
+       ) + Mw_triposphate
+    
+    #TODO:
+    #calculate spike-in numbers (absolute) (needs amount of spikeins (in nanogram?))
+    
+    #calcualte spike-in numbers per cell (needs cell number)
+}
