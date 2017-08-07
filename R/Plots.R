836a30c55b90acbe22df3c165e50191e9001a32b
carinademel <cdemel@mpibpc.mpg.de>
2017-08-07 16:31:57 +0200

    update plotting functions

--- R/Plots.R
+++ R/Plots.R
@@ -41,36 +41,56 @@ plotSpikeinCountsVsSample <- function(spikeinCounts)
                                spikein.names[rows$labeledSpikein == FALSE])
     xyplot(counts ~ Var2, scales=list(y=list(log=2)), data=df, pch=16, 
            col=spikein.colors[as.character(df$Var1)], ylab="Spike-in Counts", 
-           xlab="Sample", key=list(space="bottom", columns=2,
+           xlab="Sample", key=list(space="right", columns=2, title="Spike-ins",
                                    points=list(col=spikein.colors, pch=16),
-                                   text=list(names(spikein.colors))
-           ))
+                                   text=list(names(spikein.colors))))
 }
 
 
 
-#' @description \code{plotResultsReplicates} 
+#' @description \code{plotResultsReplicates} plots pairwise comparisons for all
+#' replicates/replicate combinations used for estimating, separated by condition
+#' and rate type from a \code{rCubeExperiment} object.
 #'
 #'
 #' @export
 #' @rdname diagnosticPlots
-#' @import graphics
+#' @import ggplot2
+#' @importFrom data.table melt
+#' @importFrom stringr str_extract
 #'
 #' @examples
 #' data(exonRates)
 #' plotResultsReplicates(exonRates)
-plotResultsReplicates <- function(featureRates){
-    # nplots <- expand.grid(cond=unique(colData(featureRates)$condition), rate=c('synthesis', 'degradation'))
-    
-    #TODO
-    
-    for(cond in unique(colData(featureRates)$condition)){
-        for(rate in c('synthesis', 'degradation')){
-            rates <- assay(featureRates)[, featureRates$rate == rate & featureRates$condition == cond]
-            pairs(rates)
-        }
+plotResultsReplicates <- function(featureRates, condition=NULL, replicate=NULL){
+    rrates <- assay(featureRates)
+    if(is.null(rownames(rrates))){
+        rownames(rrates) <- 1:nrow(rrates)
     }
+    df <- suppressMessages(melt(data.frame(rrates, gene=rownames(rrates))))
+    df$condition <- stringr::str_extract(as.character(df$variable), '[^_]+')
+    df$rate <- factor(stringr::str_extract(as.character(df$variable), '(?<=_)[^_]*[^_]'))
+    df$replicate <- stringr::str_extract(as.character(df$variable), '(?<=_)[^_]*$')
+    df <- df[,-which(colnames(df)=="variable")]
+    df2 <- reshape(df, idvar=c("gene","rate", "condition"), timevar="replicate", direction="wide")
     
+    repCombis = combn(unique(df$replicate),2)
+    
+    df3 <- do.call("rbind", lapply(1:ncol(repCombis), function(i){
+        x <- cbind(df2[,c("gene","condition","rate", paste("value", repCombis[,i], sep="."))], paste(repCombis[,i], collapse="/") )
+        colnames(x)[4:6] <- c("rate1", "rate2", "combi")
+        x
+    }))
+    
+    suppressWarnings(print(ggplot(df3, aes(x=rate1, y=rate2)) + 
+                               geom_point() + 
+                               facet_grid(rate ~ condition+combi) + 
+                               scale_x_log10() + 
+                               scale_y_log10() + 
+                               geom_abline(intercept=0, slope=1) + 
+                               xlab("") +
+                               ylab("") +
+                               ggtitle("Correlation of replicate estimations")))
 }
 
 
@@ -79,21 +99,28 @@ plotResultsReplicates <- function(featureRates){
 #' samples.
 #'
 #' @param featureCounts A \code{rCubeExperiment} object containing read counts
-#' and sample information.
+#' and sample information, as well as estimated sequencing depth and 
+#' cross-contamination rates (see \code{\link{estimateSizeFactors}}).
 #' @param featureRates A \code{rCubeRates} object containing estimated labeled
 #' and total RNA amounts for all desired replicates/conditions.
 #' @param condition (optional) Character vector indicating subset of samples
 #' for which plot should be produced
 #' @param replicate (optional) replicate/combination for which plot should be produced
 #'
-#'
 #' @export
 #' @import ggplot2
 #' @rdname diagnosticPlots
 #'
 #' @examples
+#' data(exonCounts)
+#' data(spikeinCounts)
+#' exonCounts <- estimateSizeFactors(featureCounts=exonCounts, 
+#' spikeinCounts=spikeinCounts, method="spikeinGLM")
 #' data(exonRates)
-#' plotFittedCounts(exonCounts, exonRates, "ActivatedJurkat", "1")
+#' # plot only for ActivatedJurkat condition and one replicate
+#' plotFittedCounts(exonCounts, exonRates, condition="ActivatedJurkat", replicate="1")
+#' # plot for all conditions and estimated replicates/replicate combinations
+#' plotFittedCounts(featureCounts=exonCounts, featureRates=exonRates)
 plotFittedCounts <- function(featureCounts, featureRates, condition=NULL, replicate=NULL){
     res.batches <- unique(data.frame(cond=colData(featureRates)$condition, rep=colData(featureRates)$replicate))
     
@@ -107,6 +134,7 @@ plotFittedCounts <- function(featureCounts, featureRates, condition=NULL, replic
     
     crossCont <- colData(featureCounts)$cross.contamination
     seqDepths <- colData(featureCounts)$sequencing.depth
+    stopifnot(!is.null(seqDepths) | !is.null(crossCont))
     
     reps = do.call("rbind", sapply(1:nrow(res.batches), function(r){ rep=unlist(strsplit(as.character(res.batches[r,]$rep), ':'));
     return(suppressWarnings(data.frame(res.batches[r,], repind=rep)))
