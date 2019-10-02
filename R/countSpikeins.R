346f3080a1184a2bed8819967dec2c5fa687c0e1
leo <wachutka@in.tum.de>
2019-10-02 14:11:23 +0200

    Updated count features to use biocParallel over chromosomes

--- R/countSpikeins.R
+++ R/countSpikeins.R
@@ -23,23 +23,39 @@ countSpikeins <- function(experimentalSetup, scanBamParam=Rsamtools::ScanBamPara
             message(date(), ' Counting reads from ', fn)
         }
         
-        counts <- .countSpike(bamFile=fn, region=region, scanBamParam=scanBamParam)
+        counts <- .countSpike(bamFile=fn, region=region, scanBamParam=scanBamParam, BPPARAM)
         assays(experimentalSetup[, experimentalSetup[['filename']] == fn])[['counts']] <- as.matrix(counts, ncol=1)
     }
     return(experimentalSetup) 
 }
 
 
-.countSpike <- function(bamFile, region, scanBamParam)
+.countSpike <- function(bamFile, region, scanBamParam, BPPARAM = NULL,ncores=1)
 {
-    Rsamtools::bamWhich(scanBamParam) <- region
+	
+    #Rsamtools::bamWhich(scanBamParam) <- region
+	chrs <- unique(seqnames(region))
+	param <- data.table(expand.grid(chromosome=chrs, bamFile=bamFile,
+					stringsAsFactors=FALSE))
+	
+	if(is.null(BPPARAM)){
+		BPPARAM <- MulticoreParam(workers=ncores, progressbar=TRUE, log=FALSE)
+	}
+	
     #this will split the Readpairs by CIGAR and merge(union) the resulting reads to avoid double counting of the two ends
-    exploded_reads <- GenomicAlignments::readGAlignmentPairs(bamFile, param=scanBamParam)
-    exploded_reads <- reduce(grglist(exploded_reads))
-    count <- countOverlaps(region, exploded_reads, minoverlap=2)
-    return(count)
+	bptasks(BPPARAM) <- nrow(param)
+	res <- rCube:::.bpdtapply(param, .countSpikeChunk, 
+			scanBamParam=scanBamParam, BPPARAM=BPPARAM)
+    return(Reduce(f= '+',res))
 }
-
+.countSpikeChunk <- function(chromosome, bamFile, scanBamParam, BPPARAM)
+{
+	Rsamtools::bamWhich(scanBamParam) <- GRanges(seqnames=chromosome, ranges=IRanges(0, 536870912))
+	exploded_reads <- GenomicAlignments::readGAlignmentPairs(bamFile, param=scanBamParam)
+	exploded_reads <- reduce(grglist(exploded_reads))
+	count <- countOverlaps(region, exploded_reads, minoverlap=2)
+	return(count)
+}
 
 #' @rdname Counting
 #' @export
